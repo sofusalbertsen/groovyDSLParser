3bf47909e605a69bc3b979c5d4dab91b193c5700
Thierry Lacour <thi@praqma.net>
2016-04-06 14:16:20 +0200

    Fleshed out the examples a bit

--- src/main/java/GroovyScriptEngine.java
+++ src/main/java/GroovyScriptEngine.java
@@ -1,24 +1,17 @@
 import groovy.lang.GroovyShell;
 import org.codehaus.groovy.control.CompilerConfiguration;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import java.io.*;
-import java.util.List;
+import java.io.File;
 
 public class GroovyScriptEngine {
     public static void main(String[] args) throws Exception {
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("groovy");
-        ClassLoader cl = GroovyScriptEngine.class.getClassLoader();
-        System.out.println(cl.getResource("test.dsl").getFile());
-
         // Prepare shell
         CompilerConfiguration config = new CompilerConfiguration();
         config.setScriptBaseClass("Base");
         GroovyShell shell = new GroovyShell(config);
 
         // Run script
+        ClassLoader cl = GroovyScriptEngine.class.getClassLoader();
         File myFile = new File(cl.getResource("test.dsl").getFile());
         Object result = shell.evaluate(myFile);
     }
